0f5d99db076e7640fe96015b160d51bb19b39ae4
Marhabo <maboshka753@gmail.com>
2026-06-12 00:35:53 +0500

    add layout structure with header and footer

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import Footer from "../widget/footer/footer";
+import Header from "../widget/header/header";
+import "../shared/styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
